865a427e75aeca85c5f603e4a1aa2e9a05b1a4af
shlfung <stephen.shlfung@gmail.com>
2020-12-07 22:26:44 -0800

    Basic Main Form implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import FormMain from './components/FormMain';
 import FormMedicalDayUnit from './components/FormMedicalDayUnit';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
-    <div>
+
+    <div className="container pt-4 mt-3 mb-3">
       <FormMain />
 
-    <FormMedicalDayUnit /></div>
-    
+    </div>
   );
 }
 
--- src/components/FormMain.js
+++ src/components/FormMain.js
@@ -1,46 +1,57 @@
 import { React } from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap'
+import { Card, Form, Button, Row, Col } from 'react-bootstrap'
 
 const FormMain = () => {
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        console.log('Submitted')
+    }
+
     return (
-        <div>
-            <Form>
-                <Form.Group controlId="formProjectTitle">
-                    <Form.Label>Project Title</Form.Label>
-                </Form.Group>
+        <>
+
+                    <h2 className="text-center mb-4">Main Form</h2>
+                    <Form onSubmit={handleSubmit}>
 
 
-                <Form.Group as={Row}>
-                    <Form.Label as="legend" column sm={2}>
-                        Where will the study activities take place? Please be sure to select all program areas that the participant recruitment and study visits will take place.
-                    </Form.Label>
-                    <Col sm={10}>
-                        <Form.Check
-                            type="checkbox"
-                            label="Health Information Management"
-                            name="formHorizontalRadios"
-                            id="formHorizontalRadios1"
-                        />
-                        <Form.Check
-                            type="checkbox"
-                            label="Emergency Medicine"
-                            name="formHorizontalRadios"
-                            id="formHorizontalRadios2"
-                        />
-                        <Form.Check
-                            type="checkbox"
-                            label="Medical Day Unit"
-                            name="formHorizontalRadios"
-                            id="formHorizontalRadios3"
-                        />
-                    </Col>
-                </Form.Group>
+                        <Form.Group controlId="formProjectTitle">
+                            <Form.Label><b>Project Title</b></Form.Label>
+                            <Form.Control type="text" required />
+                        </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </div>
+
+                        <Form.Group>
+                            <Form.Label>
+                                <b>Where will the study activities take place? Please be sure to select all program areas that the participant recruitment and study visits will take place.</b>
+                            </Form.Label>
+                        
+                                <Form.Check
+                                    type="checkbox"
+                                    label="Health Information Management"
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios1"
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="Emergency Medicine"
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios2"
+                                />
+                                <Form.Check
+                                    type="checkbox"
+                                    label="Medical Day Unit"
+                                    name="formHorizontalRadios"
+                                    id="formHorizontalRadios3"
+                                />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+
+        </>
     )
 }
 

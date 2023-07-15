b5221b0b6183ee6e132a6c691fec41b39688f7e1
Stephen Fung <sfung1@bcchr.ca>
2023-07-15 14:43:20 -0700

    Registered project model with admin and added label

--- projects/models.py
+++ projects/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=1000)
-    reb_num = models.CharField(max_length=1000)
+    title = models.CharField(max_length=1000, verbose_name="Project Title")
+    reb_num = models.CharField(max_length=1000, verbose_name="REB Number")
     created_at = models.DateTimeField(auto_now_add=True)

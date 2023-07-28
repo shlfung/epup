906fa5128fd58eaba0f49ade6e5db21181f023a8
Stephen Fung <sfung1@bcchr.ca>
2023-07-28 15:13:21 -0700

    Example of how to modify the Model Form with django crispy form

--- projects/forms.py
+++ projects/forms.py
@@ -1,8 +1,25 @@
 from django.forms import ModelForm
-
+from django import forms
 from .models import Project
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Row
+
 class ProjectForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_class = 'blue'
+        self.helper.label_class = 'red'
+        self.helper.css_class = 'yellow'
+
+        self.helper.layout = Layout(
+            Row(
+                Field('title', wrapper_class='green-text'),
+                Field('reb_num', css_class='bg-santared mw-100'),
+                css_class='form-row',
+            )
+        )
     class Meta:
         model = Project
         fields = ['title', 'reb_num', 'creator']

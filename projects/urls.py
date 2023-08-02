62e958662fe8c88e013d4d3e7f4eb3d518161d0d
Stephen Fung <sfung1@bcchr.ca>
2023-08-02 10:20:05 -0700

    Cleaned up the links of create project

--- projects/urls.py
+++ projects/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('projects/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail'),
     path("projects/", views.ProjectListView.as_view(), name='projects'),
     #path('projects/create/', views.ProjectCreateView.as_view(), name='project-create'),
-    path('projects/create/', views.create, name='project-create'),
+    path('create/', views.create, name='project-create'),
     #path('projects/update/<int:pk>', views.ProjectUpdateView.as_view(), name='project-update')
     path('projects/update/<int:id>', views.update, name='project-update')
 ]
